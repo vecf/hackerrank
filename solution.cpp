a1dc5ce0e1bc17122fdb453982c6b4dc31e59f4f
vecf <vaneeden.cf@gmail.com>
2022-07-02 17:55:58 -0400

    Passing all test cases.

--- solution.cpp
+++ solution.cpp
@@ -1,59 +1,115 @@
+/* #define spacefirst */
+/* #define after */
+/* #define spaceoutput */ 
+
 class LRUCache : Cache {
     
     public:
     LRUCache(int capacity) {
         this->cp = capacity;
+#ifdef spacefirst //create fist blank line
+				cout << endl;
+#endif
     }
     
     void set(int key, int value) {
-        Node* node =  new Node(key, value);
-    
-        // the list is empty
-        if ( mp.size() == 0 ) {
-            cout << "list is empty, adding first item " << key << "->" << value << "\n";
-            tail = head = node;
-            mp.insert(pair<int, Node*>(key, node));
-            return;
-        }
-        
-        cout << "map size is " << mp.size() << "/" << cp << "\n" ;
+			Node* cur = tail;
+#ifdef spaceoutput //create blank line for every set
+				cout << endl;
+#endif
 
-        // the list is full
         if ( mp.size() >= cp ) {
-            cout << "list is full " << endl;
-
-            Node* prev_node = tail->prev;
-            cout << "dropping item " << tail->key << "->" << tail->value << "\n";
-            //prev_node->next = nullptr; // causes segfault
+			/* cout << "list full" << endl; */
+            mp.erase(tail->key);
             
-            mp.erase(key);
-            
-            //prev_node->next = node; // causes segfault
-            node->prev = prev_node;
-            cout << "adding first item " << key << "->" << value << "\n";
-            mp.insert(pair<int, Node*>(key, node));
-            return;
-        } else {
-            // the list is not full
-            cout << "adding item to the tail " << key << "->" << value << "\n";
-
-            tail->next = node;
-            // cout << "node->prev = tail\n";
-
-            node->prev = tail;
-            // cout << "mp.insert\n";
-            mp.insert(pair<int, Node*>(key, node));
-            
-            // cout << "end set\n";
-            return;
+            Node* new_tail = tail->next;
+            new_tail->prev = nullptr; 
+						tail = new_tail;
         }
-        
+				
+
+		Node* node =  new Node(key, value);
+	
+		if ( mp.size() == 0 ) {
+			/* cout << "list empty" << endl; */
+			tail = head = node;
+			mp.insert(pair<int, Node*>(key, node));
+			goto done;
+			/* return; */
+		}
+
+		if ( mp.count(key) == 0) {
+			/* cout << "key not in map";// << endl; */
+
+			node->prev = head;
+			head->next = node;
+			head = node; 
+
+			mp.insert(pair<int, Node*>(key, node));
+			goto done;
+			/* return; */
+		} else {
+
+#ifdef before //for debugging
+			cout << "mp.size()= " << mp.size() << ". Before-" ;
+			while ( cur != nullptr ) {
+				cout << cur->key << ":" << cur->value << ",";
+				cur = cur->next;
+			}
+			cout << endl;
+#endif
+
+			Node* new_head; 
+			Node* neigh_next; 
+			Node* neigh_prev;
+
+			new_head = mp[key];
+			new_head->value = value; // forgot this line 2nd
+			/* cout<<"newhead "<<new_head->key << "->" << new_head->value << endl; */
+			if (new_head != head) {
+				/* cout << "newhead not head" << endl; */
+				neigh_next = new_head->next;
+			} else {
+				/* cout << "newhead is head" << endl; */
+				// nothing more to do
+				goto done;
+				/* return; */
+			}
+			if (new_head != tail) {
+				/* cout << "newhead not tail" << endl; */
+				neigh_prev = new_head->prev;
+				neigh_next->prev = neigh_prev;
+				neigh_prev->next = neigh_next;
+			} else {
+				/* cout << "newhead is tail" << endl; */
+				neigh_next->prev = nullptr;
+				tail = neigh_next;
+			}
+
+			new_head->next = nullptr;
+			new_head->prev = head;
+			head->next = new_head; // forgot this line 1st
+			head = new_head;
+		}
+				
+		done:
+#ifdef after //for debugging
+		cur = tail;
+		while ( cur != nullptr ) {
+			cout << cur->key << ":" << cur->value << ",";
+			cur = cur->next;
+		}
+		cout << endl;
+#endif
+		return;
     }
     
     int get(int key) {
         if ( mp.count(key) < 1 ) {
             return -1;
         }
-        return mp[key]->value;
+		int value = mp[key]->value;
+		/* this->set(key,value); //refresh cache */
+        return value;
     }
 };
